2f9517278d7018dff1ccd7907ae9ee1524622948
João Pelo Braga Júnior <joao.braga@reivax.com>
2023-05-17 15:19:35 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+import Navigation from './screen/Navigation/Navigation';
+
+const MainContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  overflow: hidden;
+  margin: 0;
+  box-sizing: border-box;
+  flex-direction: column;
+`;
+
+const BottomContainer = styled.div`
+  width: 100%;
+  height: calc(100% - 78px);
+  display: flex;
+  flex-direction: row;
+`;
+
+const NavigationContainer = styled.div`
+  width: 180px;
+  overflow-y: auto;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MainContainer>
+        <BottomContainer>
+          <NavigationContainer>
+            <Navigation />
+          </NavigationContainer>
+        </BottomContainer>
+      </MainContainer>
     </div>
   );
 }
---
+++ src/screen/Navigation/Navigation.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+
+const Navigation = () => {
+    return (
+        <div>
+            Navigation menu
+        </div>
+    )
+}
+
+export default Navigation
